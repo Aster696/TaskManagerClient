aff89d19fb8d3f7506a7066c99e7b98bcb5f2f3c
ASTER <asterrebeiro69@gmail.com>
2022-12-10 11:26:17 -0500

    first commit

--- src/shared/lib/routePath.ts
+++ src/shared/lib/routePath.ts
@@ -16,7 +16,7 @@ export class routePath {
     displayTasks: string = 'display-tasks';
 // server route
     // server_url: string = 'http://localhost:5700';
-    server_url: string = 'https://dead-cyan-cougar-kit.cyclic.app';
+    server_url: string = 'https://plain-gold-pronghorn.cyclic.app';
     // server_url: string = 'https://a7-task-manager-server.herokuapp.com';
     // user route
         registerServer = '/user/register';

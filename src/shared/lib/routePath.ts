bdebb397ee02cb49d81aae1c8eb56b4f0400622b
Aster07102000 <aster.r@codezen.in>
2025-05-10 18:58:28 +0530

    fist cmmit

--- src/shared/lib/routePath.ts
+++ src/shared/lib/routePath.ts
@@ -1,44 +1,45 @@
 export class routePath {
     slase: string = '/';
-// basic route
+    // basic route
     home: string = 'home';
     about: string = 'about';
     contact: string = 'contact';
     forgotPassword: string = 'forgot-password';
     forgotPasswordOtp: string = 'forgot-password-otp/';
     resetPassword: string = 'reset-password/';
-// user route
+    // user route
     login: string = 'login';
     signup: string = 'signup';
     profile: string = 'profile';
-// task route
+    // task route
     addTask: string = 'add-task';
     displayTasks: string = 'display-tasks';
-// server route
+    // server route
     // server_url: string = 'http://localhost:5700';
     // server_url: string = 'https://32a5-103-171-211-202.in.ngrok.io';
-    server_url: string = 'https://plain-gold-pronghorn.cyclic.app';
+    // server_url: string = 'https://plain-gold-pronghorn.cyclic.app';
     // server_url: string = 'https://a7-task-manager-server.herokuapp.com';
+    server_url: string = 'https://taskmanagerserver-xq2i.onrender.com'
     // user route
-        registerServer = '/user/register';
-        loginServer = '/user/login';
-        updateUserServer = '/user/update-user/';//:id
-        deleteUserServer = '/user/delete-user/';//:id
-        displayUserServer = '/user/display-user/'//:id
-        subscribeToNotification = '/user/subscribe-to-notification/';//:id
-        displayNotificationsServer = '/user/display-notifications/';//:id
-        removeNotificationServer = '/user/remove-notification/';//:id
-        forgotPasswordServer = '/user/forgot-password';
+    registerServer = '/user/register';
+    loginServer = '/user/login';
+    updateUserServer = '/user/update-user/';//:id
+    deleteUserServer = '/user/delete-user/';//:id
+    displayUserServer = '/user/display-user/'//:id
+    subscribeToNotification = '/user/subscribe-to-notification/';//:id
+    displayNotificationsServer = '/user/display-notifications/';//:id
+    removeNotificationServer = '/user/remove-notification/';//:id
+    forgotPasswordServer = '/user/forgot-password';
     // task route
-        addTaskServer = '/task/add-task';
-        updateTaskServer = '/task/update-task/';//:id
-        deleterTaskServer = '/task/delete-task/';//:id
-        displayTasksServer = '/task/display-tasks/';
-        displayTaskServer = '/task/display-task/';//:id
+    addTaskServer = '/task/add-task';
+    updateTaskServer = '/task/update-task/';//:id
+    deleterTaskServer = '/task/delete-task/';//:id
+    displayTasksServer = '/task/display-tasks/';
+    displayTaskServer = '/task/display-task/';//:id
     // email route
-        getFeedbackServer = '/email/get-feedback';
-        sendHotmailServer = '/email/send-hotmail';
-        sendResetPasswordOtpEmailServer = '/email/send-reset-password-otp-email';
-        sendPasswordUpdatedSuccessEmailServer= '/email/send-password-updated-success-email/';//:id
+    getFeedbackServer = '/email/get-feedback';
+    sendHotmailServer = '/email/send-hotmail';
+    sendResetPasswordOtpEmailServer = '/email/send-reset-password-otp-email';
+    sendPasswordUpdatedSuccessEmailServer = '/email/send-password-updated-success-email/';//:id
 
 }
